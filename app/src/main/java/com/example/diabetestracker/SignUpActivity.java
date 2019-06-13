8f0a6c560bb2106e4488f34dd5ef4481d18068a7
psn1997 <pavan.nichani.17@gmail.com>
2019-06-14 05:24:54 +0530

    Firebase Authentication connected to the registration page

--- app/src/main/java/com/example/diabetestracker/SignUpActivity.java
+++ app/src/main/java/com/example/diabetestracker/SignUpActivity.java
@@ -1,35 +1,46 @@
 package com.example.diabetestracker;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
-    EditText f_name;
-    EditText l_name;
-    EditText phone;
     EditText reg_email;
     EditText reg_password;
-    EditText reg_cnf_password;
     Button reg_sign_up;
     TextView reg_sign_in;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        f_name = (EditText) findViewById(R.id.f_name);
-        l_name = (EditText) findViewById(R.id.l_name);
-        phone = (EditText) findViewById(R.id.phone);
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
         reg_email = (EditText) findViewById(R.id.reg_email);
         reg_password = (EditText) findViewById(R.id.reg_password);
-        reg_cnf_password = (EditText) findViewById(R.id.reg_cnf_password);
         reg_sign_up = (Button) findViewById(R.id.reg_sign_up);
+        reg_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
         reg_sign_in = (TextView) findViewById(R.id.reg_sign_in);
         reg_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +49,36 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(sign_in_Intent);
             }
         });
+
+    }
+
+    private void registerUser(){
+        String email = reg_email.getText().toString().trim();
+        String password = reg_password.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter the email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter the password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering User");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this,"Registration Complete", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(SignUpActivity.this,"Could not register", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 }
